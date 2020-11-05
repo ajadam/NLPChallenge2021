2a973b160d54d685e9e1679ab36c50f9c7c1a794
Antoine Adam <antoine1.adam@etudiant.univ-rennes2.fr>
2020-11-05 14:12:48 +0100

    tf friendly dataset creation

--- code/utils/tfFriendlyTextData.py
+++ code/utils/tfFriendlyTextData.py
@@ -14,11 +14,7 @@ def makeDataset(inputDirectory, outputDirectory):
         fp.write(text)
         fp.close()
 
-df = pd.read_json(PATH+'train.json').set_index('Id')
-labels = pd.read_csv(PATH+'train_label.csv', index_col=0)
-df = pd.concat([df, labels], axis=1).drop('gender', axis=1)
+
 
 if __name__ == "__main__":
-    print(df.head())
-    print(df.Category.unique())
     # makeDataset('/Users/antoineadam/git/NLPChallenge2021/data/', '/Users/antoineadam/git/NLPChallenge2021/data/')
